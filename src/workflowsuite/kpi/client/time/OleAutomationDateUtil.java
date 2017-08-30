24751783b93b7a097dee03b2a329f37969186467
Denis Kulikov <denis.kulikov@mts.ru>
2017-08-30 13:09:17 +0300

    OleAutomationDateUtil declare constructor after constants

--- src/workflowsuite/kpi/client/time/OleAutomationDateUtil.java
+++ src/workflowsuite/kpi/client/time/OleAutomationDateUtil.java
@@ -3,11 +3,6 @@ package workflowsuite.kpi.client.time;
 import java.time.Instant;
 
 final class OleAutomationDateUtil {
-
-    private OleAutomationDateUtil() {
-
-    }
-
     private static final long MILLIS_PER_SECOND = 1000;
     private static final long MILLIS_PER_MINUTE = MILLIS_PER_SECOND * 60;
     private static final long MILLIS_PER_HOUR = MILLIS_PER_MINUTE * 60;
@@ -25,6 +20,10 @@ final class OleAutomationDateUtil {
     private static final long DAYS_TO_1899 = DAYS_PER_400_YEARS * 4 + DAYS_PER_100_YEARS * 3 - 367;
     private static final long DAYS_TO_1970 =
             DAYS_PER_400_YEARS * 4 + DAYS_PER_100_YEARS * 3 + DAYS_PER_4_YEARS * 17 + DAYS_PER_YEAR; // 719,162
+
+    private OleAutomationDateUtil() {
+
+    }
 
     static Instant fromOADate(double time) {
         //if (time >= 2958466 || time <= -657435)
